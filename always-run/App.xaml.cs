6c3874e61aa62d1b598e0fa0d82008ba74eea9d4
Alan <langwdalan@gmail.com>
2018-09-29 15:59:43 +0800

    更新

--- always-run/App.xaml.cs
+++ always-run/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,42 @@ namespace always_run
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get Reference to the current Process
+            Process thisProc = Process.GetCurrentProcess();
+            // Check how many total processes have the same name as the current one
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                // If ther is more than one, than it is already running.
+                MessageBox.Show("已有一个程序正在运行");
+                Application.Current.Shutdown();
+                return;
+            }
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            base.OnStartup(e);
+        }
+
+        //主线程异常
+        void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+                MessageBox.Show(e.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("应用程序发生不可恢复的异常，将要退出！");
+            }
+
+        }
+
+        //非主线程异常
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+        }
     }
 }
